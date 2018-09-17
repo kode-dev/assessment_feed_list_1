7ff114f00e34ec5de6d369bbaf029db8ee97703a
koopuluri <koopuluri@gmail.com>
2018-09-17 12:56:06 -0700

    whitelist document for eslint, make action constant value consistent with key, comments for configureStore.js

--- case-feed-ui/src/js/redux/actions.js
+++ case-feed-ui/src/js/redux/actions.js
@@ -1,7 +1,7 @@
 const actions = {
   Case: {
     list: {
-      FETCH_CASES: 'GET_CASES',
+      FETCH_CASES: 'FETCH_CASES',
       ADD_CASE: 'ADD_CASE',
     },
     detail: {},
--- case-feed-ui/src/js/redux/configureStore.js
+++ case-feed-ui/src/js/redux/configureStore.js
@@ -13,9 +13,16 @@ const reduxDevTool = () => {
 };
 
 export default function configureStore(initialState, history) {
+  // Router middleware
   const middleware = applyMiddleware(routerMiddleware(history));
   const asyncMiddleware = applyMiddleware(thunk);
 
+  // compose() strings together store enhancers in an elegant way.
+  // "A store enhancer is a higher-order function that composes a store creator to return a
+  // new, enhanced store creator. This is similar to middleware in that it allows you to alter
+  // the store interface in a composable way. Store enhancers are much the same concept as
+  // higher-order components in React, which are also occasionally called “component enhancers.
+  // -  - https://paulkogel.gitbooks.io/redux-docs/content/docs/Glossary.html#store-enhancer
   const composedStoreEnhancer = compose(
     middleware,
     reduxDevTool(),
@@ -24,6 +31,10 @@ export default function configureStore(initialState, history) {
 
   const store = composedStoreEnhancer(createStore)(rootReducer, initialState);
 
+  // Hot loading: "React Hot Loader is a plugin that allows React components to be live reloaded
+  // without the loss of state. It works with Webpack and other bundlers that
+  // support both Hot Module Replacement (HMR) and Babel plugins."
+  // Reference: https://gaearon.github.io/react-hot-loader/getstarted/.
   if (module.hot) {
     module.hot.accept('./rootReducers', () => {
       store.replaceReducer(rootReducer);
