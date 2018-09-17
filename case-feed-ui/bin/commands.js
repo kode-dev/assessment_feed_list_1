862c2854e7b614cff20d535d5259f88b4471777e
koopuluri <koopuluri@gmail.com>
2018-09-17 13:15:04 -0700

    remove unnecessary dependencies

--- case-feed-ui/bin/commands.js
+++ case-feed-ui/bin/commands.js
@@ -1,6 +1,5 @@
 const shell = require('shelljs');
 const config = require('config');
-const colors = require('colors');
 
 const host = config.get('host') || 'localhost';
 const port = config.get('port') || '8080';
@@ -9,17 +8,20 @@ const option = process.argv[2];
 
 switch (option) {
   case 'lint':
-    shell.exec('cross-env eslint src/js/** server/** --format node_modules/eslint-friendly-formatter . --ext .js --ext .jsx  --cache; exit 0');
+    shell.exec(
+      'cross-env eslint src/js/** server/** --format node_modules/eslint-friendly-formatter . --ext .js --ext .jsx  --cache; exit 0',
+    );
     break;
   case 'dev':
-    shell.exec(`cross-env HOST=${host} PORT=${port} webpack-dev-server --hot --progress --inline --colors --content-base ./docroot`);
+    shell.exec(
+      `cross-env HOST=${host} PORT=${port} webpack-dev-server --hot --progress --inline --colors --content-base ./docroot`,
+    );
     break;
   case 'build':
-    shell.exec(`cross-env rimraf docroot && webpack --progress`);
+    shell.exec('cross-env rimraf docroot && webpack --progress');
     break;
   default:
     // If the app type is invalid, stop execution of the file.
-    console.log(colors.green('Invalid option.'));
-    console.log(colors.green('See README.md for more details.'));
-    return;
+    console.log('Invalid option.');
+    console.log('See README.md for more details.');
 }
